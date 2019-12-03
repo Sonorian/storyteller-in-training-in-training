c63ae385913ddc117688ee0c7d7c70e6e4e9b4f7
Sonorian <quinnarmentrout@gmail.com>
2019-12-02 20:21:06 -0800

    Initial commit

---
+++ Storyteller in Training in Training.py
@@ -0,0 +1,89 @@
+import discord#https://discordpy.readthedocs.io/en/latest/index.html
+import logging
+
+logging.basicConfig(level=logging.INFO)
+client = discord.Client()#c/p from above link, idk what it does
+
+state = 'added'
+channels = []
+day = 0
+setupchannel = None
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))#when this pops up in terminal you can use the bot
+
+@client.event
+async def on_message(message):#looks at every message sent in the server
+    global state
+    global channels
+    global day
+    global setupchannel#asynchronous functions are hard
+
+    if message.author == client.user:#so it doesn't look at its own messages
+        return
+
+    if message.content.startswith('!setup') and state != 'channel':
+        if not setupchannel:
+            setupchannel = message.channel#sets one channel to be used for all admin commands
+            await message.channel.send('Setup channel has been set to `' + str(setupchannel) + '`. This is permanent.')#if you're new to the library [channel].send('msg') sends msg in [channel]
+        if setupchannel == message.channel:
+            state = 'channel'
+            await message.channel.send('Please type `!add` or `!remove` in any channel, or type `!catadd` to add all channels in its category. Type `!done` when finished')
+        else:
+            await message.channel.send('Please use `!setup` in `' + str(setupchannel) + '`')
+    if state == 'channel':
+        if message.content.startswith('!add'):
+            if message.channel in channels:
+                await message.channel.send('This channel has already been added')
+                return
+            channels.append(message.channel)#adds a specific channel to the list to be divided.
+            await message.channel.send('Added channel `' + str(channels[-1]) + '`')
+        if message.content.startswith('!catadd'):
+                if message.channel.category:#if the channel belongs to a category
+                    cats = message.channel.category.text_channels
+                    for chan in cats:
+                        if chan not in channels:
+                            channels.append(chan)#adds each channel that isn't already on the list
+                    await message.channel.send('All channels in category `' + str(message.channel.category) + '` have been added.')
+                else:
+                    await message.channel.send('This channel does not appear to be part of a category; please use `!add`')
+        if message.content.startswith('!remove'):
+            if message.channel not in channels:
+                await message.channel.send('This channel has not been added')
+                return
+            channels.remove(message.channel)#removes the channel from the list
+            await message.channel.send('Removed channel `' + str(message.channel) + '`')
+        if message.content.startswith('!done') and message.channel == setupchannel:
+            await message.channel.send('Channels set. Use `!divide` to send day dividers, `!newgame` to start a new game, and `!reset [digit]` to manually set the day.')#this sometimes breaks, idk why
+            state = 'dividing'
+    if message.channel == setupchannel:
+        if message.content.startswith('!divide'):
+            if state != 'dividing':
+                await message.channel.send('Please finish setup via either `!setup` or `!done`.')
+                return
+            if day == 0:
+                msg = ('————————— NEW GAME —————————')
+            else:
+                msg = ('————————— DAY ' + str(day) + ' —————————')#i really need to figure out how to do this more cleanly
+            for chan in channels:#where the magic happens
+                await chan.send(msg)#puts the day divider in every channel slated to be divided
+            day += 1
+        if message.content.startswith('!newgame'):
+            if state != 'dividing':
+                await message.channel.send('Please finish setup via either `!setup` or `!done`.')
+                return
+            day = 0
+            await message.channel.send('A new game has started, and the day counter has been reset. Please use `!divide` to send new game dividers')
+        if message.content.startswith('!reset'):
+            if state != 'dividing':
+                await message.channel.send('Please finish setup via either `!setup` or `!done`.')
+                return
+            digits = ['0','1','2','3','4','5','6','7','8','9']
+            if message.content[-1] not in digits:#i'm sure there's a better way to do this
+                await message.channel.send('Please enter a valid digit')
+                return
+            day = int(message.content[-1])
+            await message.channel.send('Day has been set to ' + str(day) + '. Please use `!divide` to send day dividers')
+
+client.run('NjUxMTg3MjE2OTMyMDEyMDQz.XeWSEQ.v5ffOjdTZ0pJ7QMdMgdj0cesm-k')
